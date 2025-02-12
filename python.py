675b3dcd9e69ab9029de7c0ee89559d4811f8cf5
aliHdedeh-dev <hdedehali@gmail.com>
2025-02-12 23:37:08 +0200

    ticket prices

---
+++ python.py
@@ -0,0 +1,10 @@
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("ticket price is 10$ .")
+elif age < 18:
+    print("ticket price is 5$ .")
+elif age < 13:
+    print("ticket price is 1$ .")
+elif age < 5:
+    print("ticket is free .")
